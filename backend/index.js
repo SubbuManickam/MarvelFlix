4439590b3d55c06e16dc37138c55d464894a2de9
Subbu Manickam <manickam.s@northeastern.edu>
2023-06-21 14:32:51 -0400

    Vercel changes

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,12 @@ dotenv.config();
 mongoose.connect(process.env.MONGODB_URL).then(() => console.log("Database connection successful!")).catch(err => console.log(err));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://marvel-flix-theta.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Headers');
     next();
   });
-  
+
 app.use(express.json());
  
 app.use("/api/auth", authRoute);
